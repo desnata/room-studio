d65a0f68fb7a367864fbe1d6b1c0b2ac4c2c1255
desnata <gededesnata04@gmail.com>
2026-04-09 14:40:56 +0800

    git commit -m "Refactor logout functionality to use Firebase Authentication and update file extensions to .html"

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'config.php';
-$_SESSION = array();
-if (isset($_COOKIE[session_name()])) {
-    setcookie(session_name(), '', time()-3600, '/');
-}
-session_destroy();
-header('Location: login.php');
-exit;
